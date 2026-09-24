41e21d88c62e0a20feb213a5917b1ad5d02d96ef
Vishnu Suresh <vishnusuresh683@gmail.com>
2026-09-24 21:17:03 +0530

    initialize frontend structure

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Welcome to MegaMart</h1>
+        <p>Your React application setup is ready and clean.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
